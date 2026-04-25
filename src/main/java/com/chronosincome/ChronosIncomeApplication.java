1cf7fe393fa97224613752b8d8fefcfb13632899
Guilherme Mendes <89756831+guimri@users.noreply.github.com>
2026-04-25 19:15:37 -0300

    Create ChronosIncomeApplication.java

---
+++ src/main/java/com/chronosincome/ChronosIncomeApplication.java
@@ -0,0 +1,11 @@
+package com.chronosincome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosIncomeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChronosIncomeApplication.class, args);
+    }
+}
